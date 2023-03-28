3084eea19b3d1144773285f5262cf023be1e4ce4
Andrea Zapata <95andreazapata@gmail.com>
2023-03-27 19:37:37 -0500

    Adding Stack Images and Portfolio & Footer Components

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,9 +1,17 @@
 const Stack = () => {
   return (
+    <div className="mt-6">
+        <h2 className="font-poppins text-yellow-500 text-4xl text-center font-bold">My Tech Stack</h2>
+        <p className="font-poppins text-yellow-500 text-2xl text-center mt-4">I have experience with these technologies and I'm going for more!</p>
     <div>
-        <h2>My Tech Stack</h2>
-        <p>I have experience with these technologies and I'm going for more!</p>
-      
+      <img src="src\assets\vscode-icons_file-type-html.png"></img>
+      <img src="src\assets\vscode-icons_file-type-css.png"></img>
+      <img src="src\assets\vscode-icons_file-type-js-official.png"></img>
+      <img src="src\assets\logos_react.png"></img>
+      <img src="src\assets\logos_git-icon.png"></img>
+      <img src="src\assets\vscode-icons_file-type-vscode.png"></img>
+      <img src="src\assets\akar-icons_github-fill.png"></img>
+    </div>
     </div>
   )
 }
