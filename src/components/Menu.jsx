172a77ae4bee526ef44fd599a331b31332dacf84
Andrea Zapata <95andreazapata@gmail.com>
2023-03-31 22:16:34 -0500

    Adjusting all cards with projects info

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,14 @@
 const Menu = () => {
-    return (
-        <div className="text-yellow-500 mt-5 mb-20 font-poppins text-xl">
-            <ul className="flex flex-row pl-20 pt-10">
-                <li className="mr-6">Home</li>
-                <li className="mr-6">Portfolio</li>
-                <li className="mr-6">Skills</li>
-                <li className="mr-6">Contact</li>
-            </ul>
-        </div>    
-           
-    )
-}
+  return (
+    <div className='text-yellow-500 mt-5 mb-20 font-poppins text-xl'>
+      <ul className='flex flex-row pl-20 pt-10'>
+        <li className='mr-6'>Home</li>
+        <li className='mr-6'>Portfolio</li>
+        <li className='mr-6'>Skills</li>
+        <li className='mr-6'>Contact</li>
+      </ul>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,15 +1,32 @@
-import Project from "./Project"
+import Project from './Project';
 
 const Portfolio = () => {
   return (
-    <div className="mt-10">
-    <h2 className="font-poppins text-yellow-500 text-4xl text-center font-bold">Portfolio</h2>
-    <p className="font-poppins text-yellow-500 text-2xl text-center mt-4">These are some projects I have done.</p>
+    
+      <>
+        <div className='mt-10'>
+          <h2 className='font-poppins text-yellow-500 text-4xl text-center font-bold'>
+            Portfolio
+          </h2>
+          <p className='font-poppins text-yellow-500 text-2xl text-center mt-4'>
+            These are some projects I have done.
+          </p>
+        </div>
 
-    <Project />
+        <div className='flex justify-center items-center h-screen'>
+        <div className='mt-10 grid grid-cols-3 grid-rows-2 gap-4'>
+          <Project className='mx-auto' />
+          <Project className='mx-auto' />
+          <Project className='mx-auto' />
+          <Project className='mx-auto' />
+          <Project className='mx-auto' />
+          <Project className='mx-auto' />
+        </div>
+      </div>
+      </>
 
-    </div>
-  )
-}
+      
+  );
+};
 
-export default Portfolio
+export default Portfolio;
